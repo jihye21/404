f17cd020c3018204e179a568ec208de40010fd6f
sulubin <108417106+sulubin@users.noreply.github.com>
2024-12-18 14:16:15 +0900

    wer

--- 404/src/main/java/_4/controller/MemberController.java
+++ 404/src/main/java/_4/controller/MemberController.java
@@ -27,6 +27,6 @@ public class MemberController {
 	@PostMapping("memberRegist")
 	public String memberRegist1() {
 		memberRegistService.execute();
-		 return "로그인 페이지";
+		 return "로그인 페이ert지";
 	}
 }
