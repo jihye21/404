82630b1986d6462d5be6d90f6ecc8601fd0ac4f7
301-12 <301-12@DESKTOP-S018DRP>
2024-12-18 17:39:34 +0900

    암호화 및 유효성 검사

--- 404/src/main/java/_4/command/MemberCommand.java
+++ 404/src/main/java/_4/command/MemberCommand.java
@@ -3,23 +3,42 @@ package _4.command;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Data
 public class MemberCommand {
 	String memNum;
+	
+	@NotEmpty(message = "아이디를 입력해주세요")
 	String memId;
+	
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "영문자와 숫자 그리고 특수문자가 포함된 8글자 이상")
 	String memPw;
+	@NotBlank(message = "비밀번호 확인을 입력해주세요.")
 	String memPwCon;
+	
+	@NotEmpty(message = "이름을 입력해주세요")
 	String memName;
-	String memAddr;
-	String memAddrDetail;
-	String memPost;
+	
+	@NotNull(message = "생년월일을 입력해주세요.")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	Date memBirth;
+	
 	String gender;
+	
+	@NotBlank(message = "연락처을 입력하여 주세요.")
+	@Size(min = 11, max = 23)
 	String memPhone;
+	
+	@NotBlank(message = "이메일을 입력하여 주세요.")
 	String memEmail;
 	
 }
