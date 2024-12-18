2f04dd2469f2b4f10d23bb02c46854edfd75ea57
MiniProject <hfryttyy@naver.com>
2024-12-18 12:09:51 +0900

    memberRegist

--- 404/src/main/java/_4/controller/MemberController.java
+++ 404/src/main/java/_4/controller/MemberController.java
@@ -1,12 +1,18 @@
 package _4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import _4.service.member.MemberRegistService;
 
 @Controller
 @RequestMapping("member")
 public class MemberController {
+	@Autowired
+	MemberRegistService memberRegistService; 
 	
 	@RequestMapping("memberList")
 	public String memberList() {
@@ -18,4 +24,9 @@ public class MemberController {
 		return "thymeleaf/member/memberForm";
 	}
 	
+	@PostMapping("memberRegist")
+	public String memberRegist() {
+		memberRegistService.execute();
+		// return "로그인 페이지";
+	}
 }
