8234463a5ca48d3e875bac254af9473267146b34
MiniProject <hfryttyy@naver.com>
2024-12-18 11:19:13 +0900

    회원컨트롤러의 회원가입 페이지

--- 404/src/main/java/_4/controller/MainController.java
+++ 404/src/main/java/_4/controller/MainController.java
@@ -1,17 +1,12 @@
 package _4.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("member")
 public class MainController {
-	
-	@GetMapping("memberList")
-	public String memberList() {
-		return "thymeleaf/member/memberList";
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/index";
 	}
-	
-	
 }
---
+++ 404/src/main/java/_4/controller/MemberController.java
@@ -0,0 +1,21 @@
+package _4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
+public class MemberController {
+	
+	@RequestMapping("memberList")
+	public String memberList() {
+		return "thymeleaf/member/memberList";
+	}
+	
+	@GetMapping("memberRegist")
+	public String memberRegist() {
+		return "thymeleaf/member/memberForm";
+	}
+	
+}
