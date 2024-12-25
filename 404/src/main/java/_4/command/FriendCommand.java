dab4d78a7ecaaf507a55c2da53879ecdc1d58e7b
DESKTOP-8SL8PP8\hfryt <hfryttyy@naver.com>
2024-12-25 23:53:16 +0900

    친구추가 기능 미완성

---
+++ 404/src/main/java/_4/command/FriendCommand.java
@@ -0,0 +1,12 @@
+package _4.command;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class FriendCommand {
+	String memNum;
+	String friendNum;
+	Date friendRegistDate;
+}
---
+++ 404/src/main/java/_4/domain/FriendDTO.java
@@ -0,0 +1,14 @@
+package _4.domain;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+@Data
+@Alias("friendDTO")
+public class FriendDTO {
+	String memNum;
+	String friendNum;
+	Date friendRegistDate;
+}
