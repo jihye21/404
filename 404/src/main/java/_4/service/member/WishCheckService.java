ee00a2f58cb249fedb7b6efd0ae52ee49b39cdb9
jihye21 <112855236+jihye21@users.noreply.github.com>
2024-12-26 09:49:21 +0900

    로그인 안해도 가게 구경 가능

--- 404/src/main/java/_4/service/member/WishCheckService.java
+++ 404/src/main/java/_4/service/member/WishCheckService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import _4.domain.AuthDTO;
 import _4.domain.StoreDTO;
 import _4.mapper.StoreMapper;
 import _4.mapper.service.UserNumService;
@@ -18,11 +19,21 @@ public class WishCheckService {
 	
 	public void execute(StoreDTO storeDTO, HttpSession session, Model model) {
 		String storeNum = storeDTO.getStoreNum();
-		String memNum = userNumService.execute(session);
-		String wishCheck = storeMapper.WishCheck(memNum, storeNum);
 		
-		model.addAttribute("wishCheck", wishCheck);
-		
+		//auth가 null일 경우
+		AuthDTO auth = (AuthDTO) session.getAttribute("auth");
+		if(auth == null) {
+			String wishCheck = null;
+			
+			model.addAttribute("wishCheck", wishCheck);
+		}
+		//member일 경우
+		else if(auth.getGrade().equals("member")) {
+			String memNum = userNumService.execute(session);
+			String wishCheck = storeMapper.WishCheck(memNum, storeNum);
+			
+			model.addAttribute("wishCheck", wishCheck);
+		}
 	}
 
 }
