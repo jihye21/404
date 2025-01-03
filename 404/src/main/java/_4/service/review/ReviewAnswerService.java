ad986d330442685061d1d3649ea8bff9a7955557
sulubin <108417106+sulubin@users.noreply.github.com>
2025-01-03 11:25:14 +0900

    ReviewAnswerService

---
+++ 404/src/main/java/_4/service/review/ReviewAnswerService.java
@@ -0,0 +1,21 @@
+package _4.service.review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import _4.command.ReviewCommand;
+import _4.domain.ReviewDTO;
+import _4.mapper.ReviewMapper;
+
+@Service
+public class ReviewAnswerService {
+	@Autowired
+	ReviewMapper reviewMapper;
+	public void execute(String reviewNum, String reviewAnswerContents) {
+		ReviewDTO dto = new ReviewDTO();
+		dto.setReviewNum(reviewNum);
+		dto.setReviewAnswerContents(reviewAnswerContents);
+		reviewMapper.reviewAnswerInsert(dto);
+		System.out.println("아아아아아아ㅏ");
+	}
+}
