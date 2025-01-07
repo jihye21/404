ca23b5c5e198a0dbb155f92bf05cce44782f8416
jihye21 <112855236+jihye21@users.noreply.github.com>
2025-01-07 17:34:06 +0900

    employee point 지급하기 & 한 달에 한 번으로 제한

--- 404/src/main/java/_4/controller/KafkaController.java
+++ 404/src/main/java/_4/controller/KafkaController.java
@@ -1,14 +1,31 @@
 package _4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import _4.service.employee.EmpPointRegistService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("kafka")
 public class KafkaController {
+	@Autowired
+	EmpPointRegistService empPointRegistService;
 	@GetMapping("app")
-	public String app() {
+	public String app(HttpSession session) {
+		
 		return "thymeleaf/kafka/App";
+	}
+	
+	@PostMapping("pointRegist")
+	public @ResponseBody boolean pointRegist(HttpSession session, @RequestParam ("empPoint") Integer empPoint) {
+		
+		Boolean point = empPointRegistService.execute(session, empPoint);
+		return point;
 	}
 }
