878054bf2aed316e0e72a0c0e30af62b72eb1407
301-12 <301-12@DESKTOP-S018DRP>
2025-01-07 17:51:33 +0900

    멤버 삭제 및 휴면계정만들기

--- 404/src/main/java/_4/controller/MemberController.java
+++ 404/src/main/java/_4/controller/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -113,6 +114,12 @@ public class MemberController {
 		return myPassConfirmService.execute(newPw, oldPw, session);
 	}
 	
+	@GetMapping("memberDelete")
+	public String memberDelete(@PathVariable(value = "memNum") String memNum) {
+		return "thymeleaf/member/memberDeleteChk";
+	}
+	
+	@PostMapping("memberDelete")
 	
 	
 }
