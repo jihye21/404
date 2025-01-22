73504b0fb2990884b23def6db10affca3a567bf6
sulubin <108417106+sulubin@users.noreply.github.com>
2025-01-22 15:18:24 +0900

    가게 이미지 수정 오류 해결

--- 404/src/main/java/_4/service/store/StoreInfoModifyService.java
+++ 404/src/main/java/_4/service/store/StoreInfoModifyService.java
@@ -56,65 +56,72 @@ public class StoreInfoModifyService {
 		
 		String originalTotal = "";
 		String storeTotal = "";
-		if(!storeCommand.getStoreDetailImage()[0].getOriginalFilename().isEmpty()) {
-			for(MultipartFile mpf : storeCommand.getStoreDetailImage()) {
-				String originalFile = mpf.getOriginalFilename();
-				String extension = originalFile.substring(originalFile.lastIndexOf("."));
-				String storeName = UUID.randomUUID().toString().replace("-", "");
-				String storeFileName = storeName + extension;
-				File file = new File(fileDir + "/" + storeFileName);
-				try {
-					mpf.transferTo(file);
-				} catch (IllegalStateException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				originalTotal += originalFile + "/";
-				storeTotal += storeFileName + "/";
-			}
-			dto.setStoreDetailImage(originalTotal);
-			dto.setStoreDetailStoreImage(storeTotal);
-		}
-		
-		List<FileDTO> list = (List<FileDTO>) session.getAttribute("fileList");
-		StoreDTO storeDTO = storeMapper.storeSelectOne(storeCommand.getStoreNum());
 		if(storeCommand.getStoreDetailImage() != null) {
-			List<String> dbOrg = new ArrayList<>(Arrays.asList(storeDTO.getStoreDetailImage().split("[/`]")));
-			List<String> dbStore = new ArrayList<>(Arrays.asList(storeDTO.getStoreDetailStoreImage().split("[/`]")));
-			if (list != null) {
-				for (FileDTO fdto : list) {
-					for (String img : dbOrg) {
-						if (fdto.getOrgFile().equals(img)) {
-							dbOrg.remove(fdto.getOrgFile());
-							dbStore.remove(fdto.getStoreFile());
-							break;
+			if(!storeCommand.getStoreDetailImage()[0].getOriginalFilename().isEmpty()) {
+				for(MultipartFile mpf : storeCommand.getStoreDetailImage()) {
+					String originalFile = mpf.getOriginalFilename();
+					String extension = originalFile.substring(originalFile.lastIndexOf("."));
+					String storeName = UUID.randomUUID().toString().replace("-", "");
+					String storeFileName = storeName + extension;
+					File file = new File(fileDir + "/" + storeFileName);
+					try {
+						mpf.transferTo(file);
+					} catch (IllegalStateException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					originalTotal += originalFile + "/";
+					storeTotal += storeFileName + "/";
+				}
+				dto.setStoreDetailImage(originalTotal);
+				dto.setStoreDetailStoreImage(storeTotal);
+			}
+			
+			List<FileDTO> list = (List<FileDTO>) session.getAttribute("fileList");
+			StoreDTO storeDTO = storeMapper.storeSelectOne(storeCommand.getStoreNum());
+			if(storeCommand.getStoreDetailImage() != null) {
+				List<String> dbOrg = new ArrayList<>(Arrays.asList(storeDTO.getStoreDetailImage().split("[/`]")));
+				List<String> dbStore = new ArrayList<>(Arrays.asList(storeDTO.getStoreDetailStoreImage().split("[/`]")));
+				if (list != null) {
+					for (FileDTO fdto : list) {
+						for (String img : dbOrg) {
+							if (fdto.getOrgFile().equals(img)) {
+								dbOrg.remove(fdto.getOrgFile());
+								dbStore.remove(fdto.getStoreFile());
+								break;
+							}
 						}
 					}
 				}
+				for (String img : dbOrg)
+					originalTotal += img + "/";
+				for (String img : dbStore)
+					storeTotal += img + "/";
 			}
-			for (String img : dbOrg)
-				originalTotal += img + "/";
-			for (String img : dbStore)
-				storeTotal += img + "/";
-		}
-		dto.setStoreDetailStoreImage(storeTotal);
-		dto.setStoreDetailImage(originalTotal);
-		
-		
-		
-		int i = storeMapper.storeUpdate(dto);
-		if (i > 0) {
-			if (list != null) {
-				for (FileDTO fd : list) {
-					File file1 = new File(fileDir + "/" + fd.getStoreFile());
-					if(file1.exists())
-						file1.delete();
+			dto.setStoreDetailStoreImage(storeTotal);
+			dto.setStoreDetailImage(originalTotal);
+			
+			int i = storeMapper.storeUpdate(dto);
+			if (i > 0) {
+				if (list != null) {
+					for (FileDTO fd : list) {
+						File file1 = new File(fileDir + "/" + fd.getStoreFile());
+						if(file1.exists())
+							file1.delete();
+					}
+					
 				}
-				
 			}
 		}
+		
+		
+		
+		
+		
+		
+		
 	}
 }
